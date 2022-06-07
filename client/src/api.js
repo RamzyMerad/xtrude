aa51b3d987e395f05e68b3018cdf0e5cbe93d868
RamzyMerad <ramzy.merad@student.ehb.be>
2022-06-08 00:11:54 +0200

    Code part 1

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,50 @@ async function getPosts() {
   const response = await fetch(`${url}/posts`);
  return await response.json();
 }
+async function login(user) {
+  const response = await fetch(`${url}/auth`,{
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body : JSON.stringify(user)
+  });
+ return await response.json();
+}
+
+async function register(user) {
+  const response = await fetch(`${url}/users`,{
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body : JSON.stringify(user)
+  });
+ return await response.json();
+}
+async function createPost(post) {
+  const response = await fetch(`${url}/posts`,{
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body : JSON.stringify(post)
+  });
+ return await response.json();
+}
+async function getJwt(token) {
+  const response = await fetch(`${url}/users/decodeToken?token=${token}`);
+ return await response.json();
+}
+async function updateUser(user) {
+  const response = await fetch(`${url}/users`);
+ return await response.json();
+}
 
 export{
-  getPosts
+  getPosts,
+  login,
+  register,
+  createPost,
+  getJwt
 }
